ec37425f57b4235b46f1cefceb91dc78fa344093
krisrybak <kris.rybak@hotmail.co.uk>
2018-05-29 08:03:37 +0100

    Adds travis tests

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Exception\{OutOfRangeException, InvalidRangeOrderException, IllegalMatchOperationException};
 
+/**
+ * FizzBuzzTest
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
 final class FizzBuzzTest extends TestCase
 {
     /**
